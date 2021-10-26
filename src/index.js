3ba8237428ff8e9ebe3624988c6ccf0d227028e1
PyNvimDev <86599386+pynvimdev@users.noreply.github.com>
2021-10-26 17:39:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const whitespace = <br></br>;
 const element = <h1 className='HelloWorld'> A Simple React App </h1>;
 
 ReactDOM.render(element, document.getElementById('root'));
