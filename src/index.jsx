a58c80e327e687fee33291895071d7812af43b05
Sreehariqwerty <unnikrishansreehari@gmail.com>
2021-10-26 14:27:44 +0530

    initial master

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const element = <h1> Hello World </h1>;
+export default element;
+
+ReactDOM.render(element, document.getElementById('root'));
